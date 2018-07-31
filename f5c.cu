147039ec8581ae8647d9ce9514645996c20e8d77
Thomas Daniell <thomas.tomdan@outlook.com>
2018-07-31 19:18:31 +1000

    Autoformatted cuda files

--- f5c.cu
+++ f5c.cu
@@ -23,25 +23,24 @@ void* process_db_cuda(core_t* core, db_t* db) {
     return (void*)et;
 }
 
-
 // 	cudaMalloc((void**)&dev_sample, SAMPLES*KEYBYTES*sizeof(unsigned int));		checkCudaError();
 // 	cudaMalloc((void**)&dev_corelation, KEYS*KEYBYTES*sizeof(double));			checkCudaError();
 // 	cudaMalloc((void**)&dev_hammingArray, KEYS*KEYBYTES*SAMPLES*sizeof(byte));	checkCudaError();
 // 	cudaMalloc((void**)&dev_wavestat, 2*WAVELENGTH*sizeof(double));				checkCudaError();
 // 	cudaMalloc((void**)&dev_wavestat2, KEYS*KEYBYTES*WAVELENGTH*sizeof(double));checkCudaError();
 // 	cudaMalloc((void**)&dev_hammingstat, 2*KEYS*KEYBYTES*sizeof(double));		checkCudaError();
-	
+
 // 	//make all correlation values 0 at the beginning
 // 	cudaMemset(dev_corelation,0, KEYS*KEYBYTES*sizeof(double));		checkCudaError();
 // 	//copy plain text samples to GPU
 // 	cudaMemcpy(dev_sample,sample, SAMPLES*KEYBYTES*sizeof(unsigned int),cudaMemcpyHostToDevice);	checkCudaError();
-	
+
 // 	//cuda kernel configuraion parameters
 // 	dim3 grid(KEYBYTES/16,KEYS/16);
 // 	dim3 block(16,16);
 
 // 	//find hamming statistics
 // 	hammingkernel<<<grid,block>>>(dev_sample,dev_hammingArray,dev_hammingstat,SAMPLES);
-// #ifdef DEBUG		
+// #ifdef DEBUG
 // 	cudaDeviceSynchronize(); checkCudaError();
 // #endif
