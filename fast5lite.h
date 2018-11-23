c82a3d075c47681f3147b495a92ae778abe84722
lamcw <thomas@lamcw.com>
2018-11-23 14:32:16 +1100

    Reverted fast5lite.h and added config.h target.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- fast5lite.h
+++ fast5lite.h
@@ -1,7 +1,26 @@
 #ifndef FAST5LITE_H
 #define FAST5LITE_H
 
-#include <hdf5.h>
+#ifndef HAVE_CONFIG_H
+#    define HAVE_CONFIG_H
+#    include "config.h"
+#endif
+
+#ifdef HAVE_HDF5_SERIAL_HDF5_H
+#    include <hdf5/serial/hdf5.h>
+#endif
+
+#ifdef HAVE_HDF5_H
+#    include <hdf5.h>
+#endif
+
+#ifdef HAVE_HDF5_HDF5_H
+#    include <hdf5/hdf5.h>
+#endif
+
+#ifdef HAVE___HDF5_INCLUDE_HDF5_H
+#    include <hdf5.h>
+#endif
 
 #include <errno.h>
 #include <math.h>
