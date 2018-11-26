01cb16002e3288ed6242097ac4ec8d6b8c37774c
lamcw <thomas@lamcw.com>
2018-11-26 16:31:21 +1100

    Removed extra whitespace.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- scripts/test_NA12878.sh
+++ scripts/test_NA12878.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 die() {
-    local msg="${1}"
-    echo "run.sh: ${msg}" >&2
-    exit 1
+	local msg="${1}"
+	echo "run.sh: ${msg}" >&2
+	exit 1
 }
 
 handle_tests(){
@@ -26,47 +26,46 @@ reads=$testdir/"reads.fastq"
 
 
 for file in "${bamfile}" "${ref}" "${reads}"; do
-    [[ -f "${file}" ]] || die "${file}: File does not exist"
+	[[ -f "${file}" ]] || die "${file}: File does not exist"
 done
 
 if [[ "${#}" -eq 0 ]]; then
 	echo "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 ">" ${testdir}/result.txt
-    "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 > ${testdir}/result.txt
+	"${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 > ${testdir}/result.txt
 	grep -w "chr20" ${testdir}/result.txt | awk '{print $1,$2,$3,$4,$8,$9,$10}' > ${testdir}/result_exact.txt
 	grep -w "chr20" ${testdir}/meth.exp | awk '{print $1,$2,$3,$4,$8,$9,$10}' > ${testdir}/meth_exact.txt
-	diff -q ${testdir}/meth_exact.txt ${testdir}/result_exact.txt  || die "diff ${testdir}/result_exact.txt ${testdir}/meth_exact.txt failed" 
-	
+	diff -q ${testdir}/meth_exact.txt ${testdir}/result_exact.txt  || die "diff ${testdir}/result_exact.txt ${testdir}/meth_exact.txt failed"
+
 	grep -w "chr20" ${testdir}/result.txt | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}' > ${testdir}/result_float.txt
-	grep -w "chr20" ${testdir}/meth.exp | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}'  > ${testdir}/meth_float.txt	
-	
+	grep -w "chr20" ${testdir}/meth.exp | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}'  > ${testdir}/meth_float.txt
+
 	join --nocheck-order ${testdir}/result_float.txt ${testdir}/meth_float.txt	 -a 1 -a 2 | awk -v thresh=0.1 '
-		function abs(x){return (((x) < 0.0) ? -(x) : (x))} 
-		BEGIN{status=0} 
-		{
-			if(abs($2-$5)>abs(thresh*$5)+0.02 || abs($3-$6)>abs(thresh*$6)+0.02 || abs($4-$7)>abs(thresh*$7)+0.02)
-				{print $0,abs($2-$5)">"abs(thresh*$5)+0.02 ,abs($3-$6)">"abs(thresh*$6)+0.02,abs($4-$7)">"abs(thresh*$7)+0.02;status=1} 
-		} 
-		END{if(status>0){exit 1}}
-		' > ${testdir}/floatdiff.txt || handle_tests "${file}: Validation failed"		 	
+	function abs(x){return (((x) < 0.0) ? -(x) : (x))}
+	BEGIN{status=0}
+	{
+		if(abs($2-$5)>abs(thresh*$5)+0.02 || abs($3-$6)>abs(thresh*$6)+0.02 || abs($4-$7)>abs(thresh*$7)+0.02)
+			{print $0,abs($2-$5)">"abs(thresh*$5)+0.02 ,abs($3-$6)">"abs(thresh*$6)+0.02,abs($4-$7)">"abs(thresh*$7)+0.02;status=1}
+		}
+	END{if(status>0){exit 1}}
+	' > ${testdir}/floatdiff.txt || handle_tests "${file}: Validation failed"
 
 elif [[ "${#}" -eq 1 ]]; then
-    if [[ "${1}" == "valgrind" ]]; then
-
-        valgrind "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" 
-    elif [[ "${1}" == "gdb" ]]; then
-        gdb --args "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" 
-    elif [[ "${1}" == "cpu" ]]; then
-        "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  --disable-cuda=yes > result.txt	
-    elif [[ "${1}" == "cuda" ]]; then
-        "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  --disable-cuda=no > result.txt		
-    elif [[ "${1}" == "echo" ]]; then
+	if [[ "${1}" == "valgrind" ]]; then
+		valgrind "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}"
+	elif [[ "${1}" == "gdb" ]]; then
+		gdb --args "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}"
+	elif [[ "${1}" == "cpu" ]]; then
+		"${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  --disable-cuda=yes > result.txt
+	elif [[ "${1}" == "cuda" ]]; then
+		"${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  --disable-cuda=no > result.txt
+	elif [[ "${1}" == "echo" ]]; then
 		"${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  ">" result.txt
 	else
-        echo "wrong option"
+		echo "wrong option"
 		exit 1
-    fi
+	fi
 else
-    echo "wrong option"
+	echo "wrong option"
 	exit 1
 fi
 
