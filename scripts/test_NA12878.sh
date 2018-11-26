2795d3215d26d7380d5fd2532943914e23891362
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-11-26 15:11:27 +1100

    now the na12878 test scrits validation works

--- scripts/test_NA12878.sh
+++ scripts/test_NA12878.sh
@@ -6,6 +6,15 @@ die() {
     exit 1
 }
 
+handle_tests(){
+	numfailed=$(cat ${testdir}/floatdiff.txt | wc -l)
+	numcases=$(cat ${testdir}/meth_float.txt | wc -l)
+	echo "$numfailed of $numcases test cases failed."
+	failp=$(echo "$numfailed/$numcases" | bc)
+	[ $failp -gt 0 ] && die "${1}: Validation failed"
+	exit 1
+}
+
 exepath="./f5c"
 testdir="test/chr22_meth_example/"
 
@@ -21,24 +30,24 @@ for file in "${bamfile}" "${ref}" "${reads}"; do
 done
 
 if [[ "${#}" -eq 0 ]]; then
-	echo "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 ">" ${testdir}/result_exact.txt
-    "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 > ${testdir}/result_exact.txt
-	awk '{print $1,$2,$3,$4,$8,$9,$10}' ${testdir}/result.txt > ${testdir}/result_exact.txt
-	awk '{print $1,$2,$3,$4,$8,$9,$10}' ${testdir}/meth.exp > ${testdir}/meth_exact.txt
+	echo "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 ">" ${testdir}/result.txt
+    "${exepath}" -b "${bamfile}" -g "${ref}" -r "${reads}" -t 8  -K256 > ${testdir}/result.txt
+	grep -w "chr20" ${testdir}/result.txt | awk '{print $1,$2,$3,$4,$8,$9,$10}' > ${testdir}/result_exact.txt
+	grep -w "chr20" ${testdir}/meth.exp | awk '{print $1,$2,$3,$4,$8,$9,$10}' > ${testdir}/meth_exact.txt
 	diff -q ${testdir}/meth_exact.txt ${testdir}/result_exact.txt  || die "diff ${testdir}/result_exact.txt ${testdir}/meth_exact.txt failed" 
 	
-	grep -w "chr22" ${testdir}/result.txt | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}' > ${testdir}/result_float.txt
-	grep "chr22" ${testdir}/meth.exp | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}'  > ${testdir}/meth_float.txt	
+	grep -w "chr20" ${testdir}/result.txt | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}' > ${testdir}/result_float.txt
+	grep -w "chr20" ${testdir}/meth.exp | awk '{print $1$2$3$4$8$9$10"\t"$5"\t"$6"\t"$7}'  > ${testdir}/meth_float.txt	
 	
-	join --nocheck-order ${testdir}/result_float.txt ${testdir}/meth_float.txt	 -a 1 -a 2 | awk -v thresh=0.02 '
-		function abs(x){return ((x < 0.0) ? -x : x)} 
+	join --nocheck-order ${testdir}/result_float.txt ${testdir}/meth_float.txt	 -a 1 -a 2 | awk -v thresh=0.1 '
+		function abs(x){return (((x) < 0.0) ? -(x) : (x))} 
 		BEGIN{status=0} 
 		{
-			if(abs($2-$5)>thresh || abs($3-$6)>thresh || abs($4-$7)>thresh)
-				{print $0,abs($2-$5),abs($3-$6),abs($4-$7);status=1} 
+			if(abs($2-$5)>abs(thresh*$5)+0.02 || abs($3-$6)>abs(thresh*$6)+0.02 || abs($4-$7)>abs(thresh*$7)+0.02)
+				{print $0,abs($2-$5)">"abs(thresh*$5)+0.02 ,abs($3-$6)">"abs(thresh*$6)+0.02,abs($4-$7)">"abs(thresh*$7)+0.02;status=1} 
 		} 
 		END{if(status>0){exit 1}}
-		' > ${testdir}/floatdiff.txt || die "${file}: Validation failed" 	
+		' > ${testdir}/floatdiff.txt || handle_tests "${file}: Validation failed"		 	
 
 elif [[ "${#}" -eq 1 ]]; then
     if [[ "${1}" == "valgrind" ]]; then
