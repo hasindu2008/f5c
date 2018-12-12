ec8a51e046f266a2b25660c2b0875d88c0c06f89
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-12-13 08:21:31 +1100

    fix the link in the download script

--- scripts/download_na12878.sh
+++ scripts/download_na12878.sh
@@ -10,7 +10,7 @@ test -d test || mkdir test || exit 1
 cd test || exit 1
 test -d chr22_meth_example && die "test/chr22_meth_example already exists." 
 test -e f5c_na12878_test.tgz && rm f5c_na12878_test.tgz
-wget -O f5c_na12878_test.tgz "http://genome.cse.unsw.edu.au/tmp/f5c_na12878_test.tgz" || wget -O f5c_na12878_test.tgz "https://ndownloader.figshare.com/files/13327931?private_link=5dd2077f1041412a9518" || exit 1
+wget -O f5c_na12878_test.tgz "http://genome.cse.unsw.edu.au/tmp/f5c_na12878_test.tgz" || wget -O f5c_na12878_test.tgz "https://ndownloader.figshare.com/files/13784075?private_link=b04e3976eaed2225b848" || exit 1
 echo "Extracting. Please wait for a few minutes."
 tar xf f5c_na12878_test.tgz || exit 1
 rm f5c_na12878_test.tgz
