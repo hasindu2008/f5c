05b65d0c977076c6fdf8f8642e95f07a89982a21
lamcw <thomas@lamcw.com>
2018-12-18 10:31:09 +1100

    Added options to specify f5c and nanopolish output file path.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -16,6 +16,8 @@ then
 else
 	threads=8
 fi
+f5c_output=/dev/null
+nano_output=/dev/null
 
 # terminate script
 die() {
@@ -46,12 +48,14 @@ help_msg() {
 	echo "-g [ref genome]      Same as f5c -g."
 	echo "-K [n]               Same as f5c -K."
 	echo "-C                   Same as f5c --disable-cuda=no."
+	echo "-f [file]            File to save f5c output. (/dev/null)"
+	echo "-F [file]            File to save nanopolish output. (/dev/null)"
 	echo "-t [n]               Maximum number of threads. Minimum is 8."
 	echo "-T                   Disable benchmarking over an increasing number of threads."
 	echo "-h                   Show this help message."
 }
 
-while getopts b:g:r:t:K:chT opt
+while getopts b:g:r:t:K:f:F:chT opt
 do
 	case $opt in
 		b) bamfile="$OPTARG";;
@@ -66,6 +70,8 @@ do
 		K) batchsize="$OPTARG";;
 		c) clean_cache=true;;
 		C) disable_cuda=no;;
+		f) f5c_output="$OPTARG";;
+		F) nano_output="$OPTARG";;
 		h) help_msg
 		   exit 0;;
 		?) printf "Usage: %s [-c] [-h] [f5c path] [nanopolish path]" $0
@@ -89,19 +95,18 @@ rm -f *_benchmark.log
 [ $clean_cache = true ] && clear_fscache
 
 t=8
-# run benchmark
+
 if [ $thread_loop = true ]
 then
-	while [ $t -le $threads ]
-	do
-		run "/usr/bin/time -v ${f5c_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $t --secondary=yes --min-mapq=0 --print-scaling=yes -K $batchsize --disable_cuda=$disable_cuda > /dev/null 2>> f5c_benchmark.log"
-		run "/usr/bin/time -v ${nanopolish_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $t -K $batchsize > /dev/null 2>> nano_benchmark.log"
-		t=$(( t + 8 ))
-	done
-else
-	run "/usr/bin/time -v ${f5c_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $threads --secondary=yes --min-mapq=0 --print-scaling=yes -K $batchsize --disable_cuda=$disable_cuda > /dev/null 2>> f5c_benchmark.log"
-	run "/usr/bin/time -v ${nanopolish_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $threads -K $batchsize > /dev/null 2>> nano_benchmark.log"
+	t=$threads
 fi
+# run benchmark
+while [ $t -le $threads ]
+do
+	run "/usr/bin/time -v ${f5c_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $t --secondary=yes --min-mapq=0 --print-scaling=yes -K $batchsize --disable_cuda=$disable_cuda > $f5c_output 2>> f5c_benchmark.log"
+	run "/usr/bin/time -v ${nanopolish_path} call-methylation -b ${bamfile} -g ${ref} -r ${reads} -t $t -K $batchsize > $nano_output 2>> nano_benchmark.log"
+	t=$(( t + 8 ))
+done
 
 echo "f5c time"
 grep "Elapsed (wall clock) time (h:mm:ss or m:ss):" f5c_benchmark.log | cut -d ' ' -f 8 |tr ':' \\t |  awk '{if(NF==1) print; else{ if(NF==2) print(($1*60)+$2); else print(($1*3600)+($2*60)+$3)}}' | tr '\n' '\t'
