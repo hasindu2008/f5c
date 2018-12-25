05c5e31e7c23f81fc7496c4dcdfb7c669d228258
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-12-25 14:12:35 +1100

    better error messages in the install-hts script

--- scripts/install-hdf5.sh
+++ scripts/install-hdf5.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#TODO : better error checking, exit with proper error message and state save
+#Todo : use curl as a failsafe 
+
 test -e hdf5-1.10.4.tar.gz && rm hdf5-1.10.4.tar.gz 
 test -d hdf5 && rm -r hdf5
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.4/src/hdf5-1.10.4.tar.gz || exit 1
