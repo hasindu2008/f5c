f39b56f723b0f53f7a752c7d915892d73992cddb
lamcw <thomas@lamcw.com>
2019-01-11 12:14:53 +1100

    Replaced fasta parsing with more robust code from biopython.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- scripts/group-fasta-readlen.py
+++ scripts/group-fasta-readlen.py
@@ -11,10 +11,48 @@ import argparse
 from math import trunc
 
 
-class Read:
-    def __init__(self, id, read):
-        self.id = id
-        self.read = read
+def parse_fasta(handle):
+    """Iterate over Fasta records as string tuples.
+
+    For each record a tuple of two strings is returned, the FASTA title
+    line (without the leading '>' character), and the sequence (with any
+    whitespace removed). The title line is not divided up into an
+    identifier (the first word) and comment or description.
+    >>> with open("Fasta/dups.fasta") as handle:
+    ...     for values in parse_fasta(handle):
+    ...         print(values)
+    ...
+    ('alpha', 'ACGTA')
+    ('beta', 'CGTC')
+    ('gamma', 'CCGCC')
+    ('alpha (again - this is a duplicate entry to test the indexing code)', 'ACGTA')
+    ('delta', 'CGCGC')
+
+    https://github.com/biopython/biopython/blob/master/Bio/SeqIO/FastaIO.py
+    """
+    # Skip any text before the first record (e.g. blank lines, comments)
+    # This matches the previous implementation where .readline() was used
+    for line in handle:
+        if line[0] == '>':
+            title = line[1:].rstrip()
+            break
+    else:  # no break encountered
+        return  # Premature end of file, or just empty?
+
+    # Main logic
+    # Note, remove trailing whitespace, and any internal spaces
+    # (and any embedded \r which are possible in mangled files
+    # when not opened in universal read lines mode)
+    lines = []
+    for line in handle:
+        if line[0] == '>':
+            yield title, ''.join(lines).replace(" ", "").replace("\r", "")
+            lines = []
+            title = line[1:].rstrip()
+            continue
+        lines.append(line.rstrip())
+
+    yield title, ''.join(lines).replace(" ", "").replace("\r", "")
 
 
 def cli_main():
@@ -26,13 +64,11 @@ def cli_main():
     args = parser.parse_args()
 
     with args.file as file:
-        it = iter(file)
         reads = {}
-        for line in it:
-            read_str = next(it)
+        for header, seq in parse_fasta(file):
             # round down to nearest thousand
-            rounded_length = round(len(read_str), -3)
-            read = Read(line, read_str)
+            rounded_length = round(len(seq), -3)
+            read = (header, seq)
             try:
                 reads[rounded_length].add(read)
             except KeyError:
@@ -42,8 +78,8 @@ def cli_main():
         fn = '{}k_reads.fasta'.format(trunc(k / 1000))
         with open(fn, 'a') as f:
             for read in v:
-                f.write(read.id)
-                f.write(read.read)
+                f.write('>' + read[0] + '\n')
+                f.write(read[1] + '\n')
 
 
 if __name__ == '__main__':
