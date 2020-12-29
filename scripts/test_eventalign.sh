77c070362c543dfa50b612b06cd50b9360cbaca1
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2020-12-29 17:52:12 +1100

    beautified the scripts

--- scripts/test_eventalign.sh
+++ scripts/test_eventalign.sh
@@ -79,17 +79,16 @@ handle_tests2() {
 
 
 execute_test() {
-	echo "----------------comparing summaries---------------------------------------------"
+	echo "----------------comparing summaries-----------------"
 	tail -n +2 ${testdir}/eventalign.summary.exp | awk '{print $1"\t"$2"\t"$3"\tdna\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13}' > ${testdir}/nanopolish.summary.txt
 	tail -n +2 ${testdir}/f5c_event_align.summary.txt | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14}' > ${testdir}/f5c.summary.txt
 	join ${testdir}/nanopolish.summary.txt ${testdir}/f5c.summary.txt > ${testdir}/joined_results.txt || echo "Join ran into an issue. Probably just a warning."
 	awk -f  scripts/test_eventalign_summary.awk ${testdir}/joined_results.txt > ${testdir}/joined_diff.txt || handle_tests "${file}"
 
-	echo "----------------summaries are good---------------------------------------------"
-	echo "----------------comparing full results-------------------------------------------"
 	if [ $testdir = test/chr22_meth_example ]; then
 		echo "event by event test not implemented not yet implemented!"
 	else
+		echo "----------------comparing full results--------------"
 		test -d ${testdir}_big_testresults || download_ecoli_2kb_region_big_testresults
 		tail -n +2 ${testdir}_big_testresults/eventalign.exp   > ${testdir}/nanopolish.txt
 		tail -n +2 ${testdir}/result.txt  > ${testdir}/f5c.txt
--- scripts/test_eventalign_parameters.sh
+++ scripts/test_eventalign_parameters.sh
@@ -65,15 +65,13 @@ handle_tests2() {
 #2 - awk script
 #3 - threshold for allowing failures in the  event alignment output
 execute_test() {
-	echo "----------------comparing summaries---------------------------------------------"
+	echo "----------------comparing summaries-----------------"
 	tail -n +2 ${testdir}/eventalign.summary.exp | awk '{print $1"\t"$2"\t"$3"\tdna\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13}' > ${testdir}/nanopolish.summary.txt
 	tail -n +2 ${testdir}/f5c_event_align.summary.txt | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14}' > ${testdir}/f5c.summary.txt
 	join ${testdir}/nanopolish.summary.txt ${testdir}/f5c.summary.txt > ${testdir}/joined_results.txt || echo "Join ran into an issue. Probably just a warning."
 	awk -f  scripts/test_eventalign_summary.awk ${testdir}/joined_results.txt > ${testdir}/joined_diff.txt || handle_tests "${testdir}/joined_diff.txt"
-	echo "----------------summaries are good---------------------------------------------"
-	echo "----------------comparing full results-------------------------------------------"
 
-
+	echo "----------------comparing full results--------------"
 	tail -n +2 ${testdir}_big_testresults/$1  > ${testdir}/nanopolish.txt
 	tail -n +2 ${testdir}/result.txt  > ${testdir}/f5c.txt
 	paste ${testdir}/nanopolish.txt ${testdir}/f5c.txt > ${testdir}/joined_results.txt
@@ -85,15 +83,15 @@ execute_test() {
 test -d ${testdir}_big_testresults || download_ecoli_2kb_region_big_testresults
 
 ${exepath} index -d ${testdir}/fast5_files ${testdir}/reads.fasta
-echo "************************************** --print-read-names ****************************"
+echo "************************** --print-read-names ****************************"
 ${exepath} eventalign -b ${bamfile} -g ${ref} -r ${reads} --secondary=yes --min-mapq=0 -B "$max_bases" --summary ${testdir}/f5c_event_align.summary.txt --print-read-names > ${testdir}/result.txt
 execute_test "events_print_read_names.exp" "test_eventalign.awk" 5
 echo ""
-echo "************************************** --scale-events ****************************"
+echo "****************************** --scale-events ****************************"
 ${exepath} eventalign -b ${bamfile} -g ${ref} -r ${reads} --secondary=yes --min-mapq=0 -B "$max_bases" --summary ${testdir}/f5c_event_align.summary.txt --scale-events > ${testdir}/result.txt
 execute_test "events_scale_events.exp" "test_eventalign.awk" 5
 echo ""
-echo "************************************** --sam****************************"
+echo "****************************** --sam**************************************"
 ${exepath} eventalign -b ${bamfile} -g ${ref} -r ${reads} --secondary=yes --min-mapq=0 -B "$max_bases" --summary ${testdir}/f5c_event_align.summary.txt --sam > ${testdir}/result.txt
 numfailed=$(diff -y --suppress-common-lines ${testdir}_big_testresults/events.sam.exp ${testdir}/result.txt  | wc -l)
 numcases=$(wc -l < ${testdir}_big_testresults/events.sam.exp)
@@ -102,11 +100,11 @@ echo "$failp of $numcases test cases deviated."
 [ "$failp" -gt 5 ] && die "${1}: Validation failed"
 echo "Validation passed"
 echo ""
-echo "************************************** --signal-index****************************"
+echo "**************************** --signal-index********************************"
 ${exepath} eventalign -b ${bamfile} -g ${ref} -r ${reads} --secondary=yes --min-mapq=0 -B "$max_bases" --summary ${testdir}/f5c_event_align.summary.txt --signal-index > ${testdir}/result.txt
 execute_test "event_signal-index.exp" "test_eventalign_signal_index.awk" 5
 echo ""
-echo "************************************** --samples****************************"
+echo "***************************** --samples************************************"
 ${exepath} eventalign -b ${bamfile} -g ${ref} -r ${reads} --secondary=yes --min-mapq=0 -B "$max_bases" --summary ${testdir}/f5c_event_align.summary.txt --samples > ${testdir}/result.txt
 execute_test "event_samples.exp" "test_eventalign_samples.awk" 10
 echo ""
