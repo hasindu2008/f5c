bf9e404ce2cb78868cc610911d8a386198e63933
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-02-18 10:25:50 +1100

    preliminery r10 support for eventalign

--- src/f5c.h
+++ src/f5c.h
@@ -27,8 +27,8 @@
  * major hard coded parameters *
  *******************************/
 
-#define MAX_KMER_SIZE 6 //maximum k-mer size
-#define MAX_NUM_KMER 4096   //maximum number of k-mers in nucleotide model
+#define MAX_KMER_SIZE 9 //maximum k-mer size
+#define MAX_NUM_KMER 262144  //maximum number of k-mers in nucleotide model
 #define MAX_NUM_KMER_METH 15625 //maximum number of k-mers in methylated model
 //#define HAVE_CUDA 1 //if compiled for CUDA or not
 #define ALN_BANDWIDTH 100 // the band size in adaptive_banded_dynamic_alignment
