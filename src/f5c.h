63137821432597cc3aa8d1534f6ff8f3290b2da1
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-02-18 09:43:07 +1100

    mark version as dirty

--- src/f5c.h
+++ src/f5c.h
@@ -21,7 +21,7 @@
 #include <string>
 #include <vector> //required for eventalign
 
-#define F5C_VERSION "1.4"
+#define F5C_VERSION "1.4-dirty"
 
 /*******************************
  * major hard coded parameters *
