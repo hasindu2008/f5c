17bc0cafc14b40cd1a00e52e4a91ced883419e70
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2025-09-25 15:06:05 +0300

    mis300

--- test/package-rocm.sh
+++ test/package-rocm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+#rhel 8.8
+
+
 VERSION=`git describe --tags`
 make distclean
 
 scripts/install-hdf5.sh && ./scripts/install-hts.sh && ./scripts/install-zstd.sh
 ./configure --enable-localhdf5 --enable-localzstd
-make rocm=1 -j ROCM_ARCH='"--offload-arch=gfx1030 --offload-arch=gfx1100 --offload-arch=gfx900 --offload-arch=gfx906 --offload-arch=gfx908 --offload-arch=gfx90a"'
+make rocm=1 -j ROCM_ARCH='"--offload-arch=gfx1030 --offload-arch=gfx1100 --offload-arch=gfx900 --offload-arch=gfx906 --offload-arch=gfx908 --offload-arch=gfx90a --offload-arch=gfx942"'
 
 mkdir lib/
-patchelf --set-rpath '$ORIGIN/lib' f5c
+patchelf --set-rpath --force-rpath '$ORIGIN/lib' f5c
+##https://github.com/BonsonW/slorado/releases/download/v0.3.0-beta/slorado-v0.3.0-beta-x86_64-rocm-linux-binaries.tar.xz
+##5.7 rocm
 cp ~/slorado/thirdparty/torch/libtorch/lib/libamd* lib/
 cp ~/slorado/thirdparty/torch/libtorch/lib/libhsa-runtime64.so lib/
 cp ~/slorado/thirdparty/torch/libtorch/lib/libnuma.so lib/
