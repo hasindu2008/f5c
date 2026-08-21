5159a756a7e7f94ade06393659d0c3743e00072b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-08-21 22:21:49 +1000

    mark dev

--- src/f5c.h
+++ src/f5c.h
@@ -21,7 +21,7 @@
 #include <string>
 #include <vector> //required for eventalign
 
-#define F5C_VERSION "1.7"
+#define F5C_VERSION "1.7-dirty"
 
 /*******************************
  * major hard coded parameters *
